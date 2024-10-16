64b314852f95be304e5af6e2085e24956c73929e
nikodyulger <nikolasvdulgerov@gmail.com>
2024-10-16 17:24:24 +0200

    render challenge template

--- app/app.py
+++ app/app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("home/index.html")
+
+
+@app.route("/challenge")
+def challenge():
+    return render_template("challenge/challenge.html")
