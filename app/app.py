8aa9d142bdaf03875998b16e4e8043550eda1116
nikodyulger <nikolasvdulgerov@gmail.com>
2024-10-17 19:44:17 +0200

    headers for web workers required by pyscript

--- app/app.py
+++ app/app.py
@@ -3,6 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.after_request
+def add_security_headers(response):
+    response.headers["Cross-Origin-Opener-Policy"] = "same-origin"
+    response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
+    return response
+
+
 @app.route("/")
 def index():
     return render_template("home/index.html")
@@ -11,3 +18,7 @@ def index():
 @app.route("/challenge")
 def challenge():
     return render_template("challenge/challenge.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
